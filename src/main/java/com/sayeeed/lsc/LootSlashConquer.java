277abe7c0a79abdb078ff9435c9a889ec91de730
sayeeed <chipsoud@hotmail.com>
2020-07-21 21:35:36 -0400

    initial push

---
+++ src/main/java/com/sayeeed/lsc/LootSlashConquer.java
@@ -0,0 +1,12 @@
+package com.sayeeed.lsc;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LootSlashConquer implements ModInitializer
+{
+	@Override
+	public void onInitialize() 
+	{
+		
+	}
+}
